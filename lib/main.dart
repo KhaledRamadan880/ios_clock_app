0bea4da4dd77b926059321e0c78d21bfd608e7e0
KhaledRamadan880 <khaledramdan880@gmail.com>
2024-09-08 10:51:06 +0200

    Add Device Preview Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ios_clock_app/app/app.dart';
 
 void main() {
-  runApp(const IosClockApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) {
+        return const IosClockApp();
+      },
+    ),
+  );
 }
