af81533ff0cc85264626a866b6e5ea11fbfc4533
KhaledRamadan880 <khaledramdan880@gmail.com>
2024-09-08 10:45:04 +0200

    Create Project

---
+++ android/app/src/main/kotlin/com/example/ios_clock_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ios_clock_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:ios_clock_app/app/app.dart';
+
+void main() {
+  runApp(const IosClockApp());
+}
